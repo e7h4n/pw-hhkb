900cb023f46ec8b5094c4b8ac27bf9c08c4485bd
zhangyc <zhangyc@fenbi.com>
2018-01-10 19:49:35 +0800

    MOD: tidy code

--- main.c
+++ main.c
@@ -1,59 +1,3 @@
-/**
- * Copyright (c) 2012 - 2017, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-/** @file
- *
- * @defgroup ble_sdk_app_hids_keyboard_main main.c
- * @{
- * @ingroup ble_sdk_app_hids_keyboard
- * @brief HID Keyboard Sample Application main file.
- *
- * This file contains is the source code for a sample application using the HID, Battery and Device
- * Information Services for implementing a simple keyboard functionality.
- * Pressing Button 0 will send text 'hello' to the connected peer. On receiving output report,
- * it toggles the state of LED 2 on the mother board based on whether or not Caps Lock is on.
- * This application uses the @ref app_scheduler.
- *
- * Also it would accept pairing requests from any peer device.
- */
-
 #include <stdint.h>
 #include <string.h>
 #include "nordic_common.h"
@@ -95,10 +39,6 @@
 #define CENTRAL_LINK_COUNT               0                                          /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
 #define PERIPHERAL_LINK_COUNT            1                                          /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
-#define UART_TX_BUF_SIZE                 256                                        /**< UART TX buffer size. */
-#define UART_RX_BUF_SIZE                 1                                          /**< UART RX buffer size. */
-
-#define KEY_PRESS_BUTTON_ID              0                                          /**< Button used as Keyboard key press. */
 #define SHIFT_BUTTON_ID                  1                                          /**< Button used as 'SHIFT' Key. */
 
 #define DEVICE_NAME                      "Nordic_Keyboard"                          /**< Name of device. Will be included in the advertising data. */
@@ -148,7 +88,7 @@
 #define INPUT_REP_REF_ID                 0                                           /**< Id of reference to Keyboard Input Report. */
 #define OUTPUT_REP_REF_ID                0                                           /**< Id of reference to Keyboard Output Report. */
 
-#define APP_FEATURE_NOT_SUPPORTED        BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2        /**< Reply when unsupported features are requested. */
+#define APP_FEATURE_NOT_SUPPORTED        (BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2)        /**< Reply when unsupported features are requested. */
 
 #define MAX_BUFFER_ENTRIES               5                                           /**< Number of elements that can be enqueued */
 
@@ -200,15 +140,6 @@
     } while (0)
 
 /** @} */
-
-typedef enum {
-    BLE_NO_ADV,             /**< No advertising running. */
-    BLE_DIRECTED_ADV,       /**< Direct advertising to the latest central. */
-    BLE_FAST_ADV_WHITELIST, /**< Advertising with whitelist. */
-    BLE_FAST_ADV,           /**< Fast advertising running. */
-    BLE_SLOW_ADV,           /**< Slow advertising running. */
-    BLE_SLEEP,              /**< Go to system-off. */
-} ble_advertising_mode_t;
 
 /** Abstracts buffer element */
 typedef struct hid_key_buffer {
@@ -544,7 +475,7 @@ static void gap_params_init(void) {
 
     err_code = sd_ble_gap_device_name_set(&sec_mode,
                                           (const uint8_t *) DEVICE_NAME,
-                                          strlen(DEVICE_NAME));
+                                          (uint16_t) strlen(DEVICE_NAME));
     APP_ERROR_CHECK(err_code);
 
     err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_HID_KEYBOARD);
@@ -552,7 +483,7 @@ static void gap_params_init(void) {
 
     memset(&gap_conn_params, 0, sizeof(gap_conn_params));
 
-    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
+    gap_conn_params.min_conn_interval = (uint16_t) MIN_CONN_INTERVAL;
     gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
     gap_conn_params.slave_latency = SLAVE_LATENCY;
     gap_conn_params.conn_sup_timeout = CONN_SUP_TIMEOUT;
@@ -930,8 +861,8 @@ static uint32_t buffer_enqueue(ble_hids_t *p_hids,
         element = &buffer_list.buffer[(buffer_list.wp)];
         element->p_instance = p_hids;
         element->p_data = p_key_pattern;
-        element->data_offset = offset;
-        element->data_len = pattern_len;
+        element->data_offset = (uint8_t) offset;
+        element->data_len = (uint8_t) pattern_len;
 
         buffer_list.count++;
         buffer_list.wp++;
@@ -980,7 +911,7 @@ static uint32_t buffer_dequeue(bool tx_flag) {
             if ((err_code == BLE_ERROR_NO_TX_PACKETS) && (actual_len <= p_element->data_len)) {
                 // Transmission could not be completed, do not remove the entry, adjust next data to
                 // be transmitted
-                p_element->data_offset = actual_len;
+                p_element->data_offset = (uint8_t) actual_len;
                 remove_element = false;
             }
         }
@@ -1537,7 +1468,7 @@ static void advertising_init(void) {
     err_code = ble_advertising_init(&advdata,
                                     NULL,
                                     &options,
-                                    on_adv_evt,
+                                    (ble_advertising_evt_handler_t const) on_adv_evt,
                                     ble_advertising_error_handler);
     APP_ERROR_CHECK(err_code);
 }
@@ -1603,15 +1534,10 @@ int main(void) {
     advertising_start();
 
     // Enter main loop.
-    for (;;) {
+    while (1) {
         app_sched_execute();
         if (NRF_LOG_PROCESS() == false) {
             power_manage();
         }
     }
 }
-
-
-/**
- * @}
- */
