77b41ef7a65a837cf3438b7f62c0fc435c3fa561
zhangyc <zhangyc@fenbi.com>
2018-01-17 10:48:06 +0800

    MOD: use segger rtt as debug output

--- src/main.c
+++ src/main.c
@@ -1,18 +1,16 @@
 #include <nrf_gpio.h>
 #include <nrf_delay.h>
-#include <libraries/experimental_log/nrf_log.h>
-#include <libraries/experimental_log/nrf_log_ctrl.h>
-#include <libraries/experimental_log/nrf_log_default_backends.h>
+#include <SEGGER_RTT.h>
 
 int main(void) {
-    nrf_gpio_pin_dir_set(17, NRF_GPIO_PIN_DIR_OUTPUT);
-
-    NRF_LOG_INIT(NULL);
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
+    uint8_t count = 0;
+    nrf_gpio_pin_dir_set(19, NRF_GPIO_PIN_DIR_OUTPUT);
 
     while (true) {
-        nrf_gpio_pin_toggle(17);
-        nrf_delay_ms(1000);
-        NRF_LOG_INFO("hello")
+        nrf_gpio_pin_toggle(19);
+        nrf_delay_ms(2000);
+        SEGGER_RTT_printf(0, "[%d]Hello PW-HHKB!\n", count);
+
+        count = (count + 1) % 8;
     }
 }
